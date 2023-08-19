0afa39ffa7b1d4494438871d484be58ef9b649c8
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2023-08-19 19:32:59 +0200

    g

---
+++ Global Scripts/Global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var levels = null
+
+func _ready():
+	var file = FileAccess.open('res://Scenes/Userinterfaces/levels.json', FileAccess.READ)
+	levels = JSON.parse_string(file.get_as_text())
